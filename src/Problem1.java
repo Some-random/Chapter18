106d35210e62e6bb9ab66328e49371761897baa1
dongweij <jdw19930725@gmail.com>
2015-08-11 12:28:09 +0800

    Initial commit

---
+++ src/Problem1.java
@@ -0,0 +1,16 @@
+/**
+ * Created by dongweij on 2015/8/11.
+ */
+public class Problem1 {
+    public static int addTwoNums(int a, int b) {
+        if(a >= 0 && b >= 0) {
+
+        }
+        return 0;
+    }
+
+
+    public static void main(String args[]) {
+        System.out.println(addTwoNums(5,15));
+    }
+}
---
+++ src/Problem2.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+/**
+ * Created by dongweij on 2015/8/11.
+ */
+public class Problem2 {
+    public static void shuffle(int[] cards) {
+        Random rng = new Random();
+        int length = cards.length;
+        for(int i = 0; i < cards.length; i++) {
+            int temp = rng.nextInt(length);
+            int tempvalue = cards[i];
+            cards[i] = cards[temp];
+            cards[temp] = tempvalue;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] cards = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        shuffle(cards);
+        for(int i = 0; i < cards.length; i++) {
+            System.out.print(cards[i] + " ");
+        }
+    }
+}
